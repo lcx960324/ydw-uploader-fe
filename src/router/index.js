4fe078c5eb6bee82fc65f8c21860ef1726971efe
Lawrence Lee <lawrence.lee@foxmail.com>
2017-08-14 19:25:50 +0800

    add explorer

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
+import Login from '@/views/login'
+import Explorer from '@/views/explorer'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/explorer',
+      name: 'Explorer',
+      component: Explorer
     }
   ]
 })
+export default router
