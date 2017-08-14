d7645387fbfef75294bbd7cca48db74aaa62c336
Lawrence Lee <lawrence.lee@foxmail.com>
2017-08-14 15:36:46 +0800

    modify vue plugin loader

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import iView from 'iview'
 import router from './router'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 
 Vue.use(Router)
-Vue.use(iView)
 
 export default new Router({
   routes: [
