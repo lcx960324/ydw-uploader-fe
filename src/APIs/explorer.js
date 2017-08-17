2c20a882034bc765a5979c90a3d10fb392a8ec94
Lawrence Lee <lawrence.lee@foxmail.com>
2017-08-17 22:01:26 +0800

    update explorer

--- src/APIs/explorer.js
+++ src/APIs/explorer.js
@@ -1,1 +1,39 @@
 import axios from 'axios'
+import common from './common'
+const ajax = axios.create({
+  ...common,
+  baseURL: common.baseURL + '/user'
+})
+
+export const createDir = function (current_path, name) {
+  return ajax.get('/createDIR.do', {
+    params: {
+      current_path,
+      name
+    }
+  })
+}
+export const getFileList = function (current_path, dirName) {
+  return ajax.get('/fileList.do', {
+    params: {
+      current_path,
+      dirName
+    }
+  })
+}
+export const deleteDir = function (current_path, dName) {
+  return ajax.get('/deleteDIR.do', {
+    params: {
+      current_path,
+      dName
+    }
+  })
+}
+export const deleteFile = function (current_path, fname) {
+  return ajax.get('/deleteFile.do', {
+    params: {
+      current_path,
+      fname
+    }
+  })
+}
