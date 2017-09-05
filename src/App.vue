c5c755a992b0ce8394d6b267c78f947c12b73581
Lawrence Lee <lawrence.lee@foxmail.com>
2017-09-05 19:34:49 +0800

    update style

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,10 @@ export default {
 @bg-start: #0066FF;
 @bg-end: #6699CC;
 html {
-  height: 100%;
+  min-height: 100%;
 }
 body {
-  height: 100%;
+  min-height: 100%;
   background: linear-gradient(to bottom, @bg-start, @bg-end);
   padding-top: 1px;
 }
