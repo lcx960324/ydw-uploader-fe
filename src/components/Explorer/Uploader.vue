d424870dc1bc3bf908157c11aacdecb7b00fa80c
Lawrence Lee <lawrence.lee@foxmail.com>
2017-09-07 18:10:22 +0800

    uploader

--- src/components/Explorer/Uploader.vue
+++ src/components/Explorer/Uploader.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="uploader">
-    <Upload multiple type="drag" action="//jsonplaceholder.typicode.com/posts/">
+    <Upload multiple type="drag" action="http://httppost.cn:8080/user/upload.do"
+    :data="{current_path: '/'}"
+    name="file"
+    with-credentials>
       <div style="padding: 20px 0">
         <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
         <p>点击或将文件拖拽到这里上传</p>
